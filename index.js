204f48b7a17ddbd0cc338d45732bd867bc4a139d
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-21 21:13:55 +0000

    Promise style

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
 
-const questions = [
-  'name',
-  'location',
-  'bio',
-  'linkedinUrl',
-  'githubUrl',
-]
+const questions = [ 'name', 'location', 'bio', 'linkedinUrl', 'githubUrl', ]
 const inquirerQuestions = questions.map(it => {
   return {
     type: 'input',
@@ -16,27 +10,17 @@ const inquirerQuestions = questions.map(it => {
   };
 })
 
-async function askQuestions() {
-  return await inquirer.prompt(inquirerQuestions)
-}
-
-async function collectAnswersAndGenerateProfile() {
-  console.log('Answer some questions, dude.')
-  const responses = await askQuestions()
-  console.log('Generating HTML from template.')
-  await generateProfileHtml(responses)
-  console.log('All done, dude.')
-}
-
-async function generateProfileHtml(answers) {
-  const templateDocument = await fs.promises.readFile('./template.html', 'utf8')
-  const profileHtml = templateDocument
-    .replace('NAME', answers.name)
-    .replace('LOCATION', answers.location)
-    .replace('BIO', answers.bio)
-    .replace('LINKED_IN_URL', answers.linkedinUrl)
-    .replace('GITHUB_URL', answers.githubUrl)
-  await fs.promises.writeFile('./index.html', profileHtml)
-}
-
-collectAnswersAndGenerateProfile()
+inquirer.prompt(inquirerQuestions)
+  .then(answers => {
+    fs.readFile('./template.html', 'utf8', (err, templateDocument) => {
+      const profileHtml = templateDocument
+        .replace('NAME', answers.name)
+        .replace('LOCATION', answers.location)
+        .replace('BIO', answers.bio)
+        .replace('LINKED_IN_URL', answers.linkedinUrl)
+        .replace('GITHUB_URL', answers.githubUrl)
+      fs.writeFile('./index.html', profileHtml, () => {
+        console.log('DONE')
+      })
+    })
+  })
